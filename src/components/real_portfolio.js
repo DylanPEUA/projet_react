18a2b0ca7863c3ca2fadc6ff460a3c216a265a9d
Dylan Peua <dylanpeua2@gmail.com>
2025-08-11 22:36:50 +0200

    Page de portfolio en cours

---
+++ src/components/real_portfolio.js
@@ -0,0 +1,145 @@
+import React from 'react';
+
+
+function Realportfolio() {
+  return (
+    <>
+        {/* Fresh Food */}
+        <div className="border border-gray-300 rounded-md">
+            <div className="flex flex-col items-center justify-center text-white">
+                <img
+                src="/img/portfolio/fresh-food.jpg"
+                alt="Service"
+                className="w-auto h-auto"
+                style={{ maxWidth: '100%', height: 'auto' }}
+                />
+            </div>
+            <h3 className="text-xl font-bold text-gray-800 mt-2 mb-2">Fresh Food</h3>
+            <p className="text-black text-sm leading-relaxed mb-4">
+                Site de vente de produit frais en ligne
+            </p>
+            <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mb-4">
+                Voir le site
+            </button>
+           <div className="border-t border-gray-300 text-gray-700 text-sm bg-gray-100 text-center flex items-center justify-center h-8">
+                Site réalisé avec PHP et MySQL
+            </div>
+        </div>
+
+        {/* Fresh Food */}
+        <div className="border border-gray-300 rounded-md">
+            <div className="flex flex-col items-center justify-center text-white">
+                <img
+                src="/img/portfolio/fresh-food.jpg"
+                alt="Service"
+                className="w-auto h-auto"
+                style={{ maxWidth: '100%', height: 'auto' }}
+                />
+            </div>
+            <h3 className="text-xl font-bold text-gray-800 mt-2 mb-2">Fresh Food</h3>
+            <p className="text-black text-sm leading-relaxed mb-4">
+                Site de vente de produit frais en ligne
+            </p>
+            <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mb-4">
+                Voir le site
+            </button>
+           <div className="border-t border-gray-300 text-gray-700 text-sm bg-gray-100 text-center flex items-center justify-center h-8">
+                Site réalisé avec PHP et MySQL
+            </div>
+        </div>
+
+        {/* Fresh Food */}
+        <div className="border border-gray-300 rounded-md">
+            <div className="flex flex-col items-center justify-center text-white">
+                <img
+                src="/img/portfolio/fresh-food.jpg"
+                alt="Service"
+                className="w-auto h-auto"
+                style={{ maxWidth: '100%', height: 'auto' }}
+                />
+            </div>
+            <h3 className="text-xl font-bold text-gray-800 mt-2 mb-2">Fresh Food</h3>
+            <p className="text-black text-sm leading-relaxed mb-4">
+                Site de vente de produit frais en ligne
+            </p>
+            <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mb-4">
+                Voir le site
+            </button>
+           <div className="border-t border-gray-300 text-gray-700 text-sm bg-gray-100 text-center flex items-center justify-center h-8">
+                Site réalisé avec PHP et MySQL
+            </div>
+        </div>
+
+        {/* Fresh Food */}
+        <div className="border border-gray-300 rounded-md">
+            <div className="flex flex-col items-center justify-center text-white">
+                <img
+                src="/img/portfolio/fresh-food.jpg"
+                alt="Service"
+                className="w-auto h-auto"
+                style={{ maxWidth: '100%', height: 'auto' }}
+                />
+            </div>
+            <h3 className="text-xl font-bold text-gray-800 mt-2 mb-2">Fresh Food</h3>
+            <p className="text-black text-sm leading-relaxed mb-4">
+                Site de vente de produit frais en ligne
+            </p>
+            <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mb-4">
+                Voir le site
+            </button>
+           <div className="border-t border-gray-300 text-gray-700 text-sm bg-gray-100 text-center flex items-center justify-center h-8">
+                Site réalisé avec PHP et MySQL
+            </div>
+        </div>
+
+
+        {/* Fresh Food */}
+        <div className="border border-gray-300 rounded-md">
+            <div className="flex flex-col items-center justify-center text-white">
+                <img
+                src="/img/portfolio/fresh-food.jpg"
+                alt="Service"
+                className="w-auto h-auto"
+                style={{ maxWidth: '100%', height: 'auto' }}
+                />
+            </div>
+            <h3 className="text-xl font-bold text-gray-800 mt-2 mb-2">Fresh Food</h3>
+            <p className="text-black text-sm leading-relaxed mb-4">
+                Site de vente de produit frais en ligne
+            </p>
+            <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mb-4">
+                Voir le site
+            </button>
+           <div className="border-t border-gray-300 text-gray-700 text-sm bg-gray-100 text-center flex items-center justify-center h-8">
+                Site réalisé avec PHP et MySQL
+            </div>
+        </div>
+
+
+        {/* Fresh Food */}
+        <div className="border border-gray-300 rounded-md">
+            <div className="flex flex-col items-center justify-center text-white">
+                <img
+                src="/img/portfolio/fresh-food.jpg"
+                alt="Service"
+                className="w-auto h-auto"
+                style={{ maxWidth: '100%', height: 'auto' }}
+                />
+            </div>
+            <h3 className="text-xl font-bold text-gray-800 mt-2 mb-2">Fresh Food</h3>
+            <p className="text-black text-sm leading-relaxed mb-4">
+                Site de vente de produit frais en ligne
+            </p>
+            <button className="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded mb-4">
+                Voir le site
+            </button>
+           <div className="border-t border-gray-300 text-gray-700 text-sm bg-gray-100 text-center flex items-center justify-center h-8">
+                Site réalisé avec PHP et MySQL
+            </div>
+        </div>  
+
+    </>
+  );
+}
+
+export default Realportfolio;
