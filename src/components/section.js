f52aa8dc5e93ff215e7eda3ca10ba157fe1665e1
Dylan Peua <dylanpeua2@gmail.com>
2025-08-14 18:52:11 +0200

    Page de mention finalisé

---
+++ src/components/section.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { ChevronRightIcon } from "@heroicons/react/24/outline";
+
+function Section({ title, isOpen, onClick, children, className }) {
+  return (
+    <div className={`border bg-white overflow-hidden ${className}`}>
+      {/* Titre + flèche */}
+      <button
+        onClick={onClick}
+        className="flex justify-between items-center w-full px-4 py-3 text-left font-semibold hover:bg-gray-100 transition"
+      >
+        {title}
+        <ChevronRightIcon
+          className={`h-5 w-5 text-gray-500 transition-transform duration-300 ${
+            isOpen ? "rotate-90" : ""
+          }`}
+        />
+      </button>
+
+      {/* Contenu */}
+      <div
+        className={`transition-all duration-300 ease-in-out overflow-hidden ${
+          isOpen ? "max-h-96 p-4" : "max-h-0 p-0"
+        }`}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
+
+export default Section;
--- src/pages/Mentions.js
+++ src/pages/Mentions.js
@@ -1,7 +1,135 @@
-import React from 'react';
+import { useState } from "react";
+import {
+  MapIcon,
+  MapPinIcon,
+  PhoneIcon,
+  GlobeAltIcon,
+} from "@heroicons/react/24/outline";
+import Section from '../components/section';
 
 function Mentions() {
-  return <h1>Mentions</h1>;
+  const [openIndex, setOpenIndex] = useState(null);
+
+  const toggleSection = (index) => {
+    // Accordéon : si la section est déjà ouverte, on ferme tout, sinon on ouvre la nouvelle
+    setOpenIndex(openIndex === index ? null : index);
+  };
+
+  const sections = [
+    {
+      title: "Éditeur du site",
+      content: (
+        <div className="flex-1 text-left space-y-2">
+          <h2 className="text-xl font-bold mb-4">John Doe</h2>
+
+          <p className="flex items-center gap-2">
+            <MapIcon className="h-5 w-5" />
+            Ave du Prince Hinoi
+          </p>
+
+          <p className="flex items-center gap-2">
+            <MapPinIcon className="h-5 w-5" />
+            98714 Papeete, French Polynesia
+          </p>
+
+          <p className="flex items-center gap-2">
+            <PhoneIcon className="h-5 w-5" />
+            +689 87 00 00 00
+          </p>
+        </div>
+      )
+    },
+    {
+      title: "Hébergeur",
+      content: (
+        <div className="flex-1 text-left space-y-2">
+          <h2 className="font-bold text-xl">alwaysdata</h2>
+          <p>91 Rue du Faubourg Saint-Honoré, 75008 Paris</p>
+
+          <p className="flex items-center gap-2">
+            <GlobeAltIcon className="h-5 w-5" />
+            <a
+              href="https://www.alwaysdata.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline"
+            >
+              www.alwaysdata.com
+            </a>
+          </p>
+        </div>
+      )
+    },
+    {
+      title: "Crédits",
+      content: (
+        <div className="flex-1 text-left space-y-2">
+          <h2 className="font-bold text-xl">Crédits</h2>
+          <p>
+            Ce site a été réalisé par John Doe, étudiant au{" "}
+            <a
+              href="https://www.centre-europeen-formation.fr/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              Centre Européen de Formation
+            </a>
+          </p>
+
+          <p className="italic">
+            Les images utilisées sur ce site sont libres de droits et ont été obtenues sur{" "}
+            <a
+              href="https://pixabay.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              Pixabay
+            </a>
+          </p>
+
+          <p className="italic">
+            La favicon de ce site a été fournie par{" "}
+            <a
+              href="https://heroicons.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-blue-600 hover:underline"
+            >
+              Heroicons
+            </a>
+          </p>
+        </div>
+      )
+    }
+  ];
+
+  return (
+    <>
+      <div className="py-16 px-4 bg-white text-center">
+        <h2 className="text-3xl font-bold text-gray-800 ">Mentions Légales</h2>
+        <div className="w-1/4 mx-auto h-1 bg-blue-500 mt-2 mb-12" />
+      </div>
+
+      <div className="w-3/4 mx-auto">
+        {sections.map((section, index) => (
+          <Section
+            key={index}
+            title={section.title}
+            isOpen={openIndex === index}
+            onClick={() => toggleSection(index)}
+            className={`
+              ${index === 0 ? "mt-4" : ""}
+              ${index === sections.length - 1 ? "mb-8" : ""}
+            `}>
+          
+            {section.content}
+          </Section>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default Mentions;
